6c17c5b48fe48f4d475f080fea9e898e28405914
geochronology <54387644+geochronology@users.noreply.github.com>
2020-12-09 10:00:42 -0800

    completed section 1

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { CssBaseline, makeStyles } from '@material-ui/core';
 import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
 import './App.css';
@@ -20,6 +20,7 @@ function App() {
       <div className={classes.appMain}>
         <Header />
       </div>
+      <CssBaseline />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
 import { AppBar, Badge, Grid, IconButton, InputBase, Toolbar } from '@material-ui/core'
 import React from 'react'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 
 export default function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
         <Grid container>
-          <Grid item sm={4} style={{ border: '1px solid #fff' }}>
+          <Grid item>
             <InputBase />
           </Grid>
-          <Grid item sm={8} style={{ border: '1px solid #000' }}>
+
+          <Grid item sm></Grid>
+
+          <Grid item>
             <IconButton>
               <Badge badgeContent={4} color="secondary">
                 <NotificationsNoneIcon />
               </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={3} color="primary">
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <PowerSettingsNewIcon />
             </IconButton>
           </Grid>
         </Grid>
