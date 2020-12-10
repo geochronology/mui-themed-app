4d271c2e77b8cbcdc0d26e1ce4f38a2db55f2fa2
geochronology <54387644+geochronology@users.noreply.github.com>
2020-12-10 13:36:32 -0800

    added radio button controls

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextField, Grid } from '@material-ui/core'
+import { TextField, Grid, RadioGroup, FormControlLabel, Radio, FormControl, FormLabel } from '@material-ui/core'
 import { useForm, Form } from '../../components/useForm'
 
 const initialFormValues = {
@@ -41,7 +41,20 @@ export default function EmployeeForm() {
             onChange={handleInputChange}
           />
         </Grid>
-        <Grid item xs={6}></Grid>
+        <Grid item xs={6}>
+          <FormControl>
+            <FormLabel>Gender</FormLabel>
+            <RadioGroup row
+              name="gender"
+              value={values.gender}
+              onChange={handleInputChange}
+            >
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel value="female" control={<Radio />} label="Female" />
+              <FormControlLabel value="other" control={<Radio />} label="Other" />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
 
       </Grid>
     </Form>
