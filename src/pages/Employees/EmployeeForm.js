708928eedcde0135e52e3b278ebe7f86934448cb
geochronology <54387644+geochronology@users.noreply.github.com>
2020-12-13 17:18:33 -0800

    added crud setters for creating new entry from form submit

--- src/pages/Employees/EmployeeForm.js
+++ src/pages/Employees/EmployeeForm.js
@@ -53,7 +53,10 @@ export default function EmployeeForm() {
 
   const handleSubmit = e => {
     e.preventDefault()
-    if (validate()) window.alert('testing')
+    if (validate()) {
+      employeeService.insertEmployee(values)
+    }
+    resetForm()
   }
 
   return (
