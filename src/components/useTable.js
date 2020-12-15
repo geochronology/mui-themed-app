d0210d45e365684950b45d75504fb8b7feaa4154
geochronology <54387644+geochronology@users.noreply.github.com>
2020-12-15 15:31:43 -0800

    added and implemented sorting function

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,4 +1,4 @@
-import { makeStyles, Table, TableCell, TableHead, TablePagination, TableRow } from '@material-ui/core'
+import { makeStyles, Table, TableCell, TableHead, TablePagination, TableRow, TableSortLabel } from '@material-ui/core'
 import React, { useState } from 'react'
 
 const useStyles = makeStyles(theme => ({
@@ -26,6 +26,8 @@ export default function useTable(records, headCells, filterFn) {
   const pages = [5, 10, 25]
   const [page, setPage] = useState(0)
   const [rowsPerPage, setRowsPerPage] = useState(pages[0])
+  const [order, setOrder] = useState()
+  const [orderBy, setOrderBy] = useState()
 
 
   const TableContainer = (props) => (
@@ -36,13 +38,25 @@ export default function useTable(records, headCells, filterFn) {
 
   const TableHeader = props => {
 
+    const handleSortRequest = cellId => {
+      const isAsc = orderBy === cellId && order === "asc"
+      setOrder(isAsc ? 'desc' : 'asc')
+      setOrderBy(cellId)
+    }
+
     return (
       <TableHead>
         <TableRow>
           {
             headCells.map(headCell => (
               <TableCell key={headCell.id}>
-                {headCell.label}
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : 'asc'}
+                  onClick={() => { handleSortRequest(headCell.id) }}
+                >
+                  {headCell.label}
+                </TableSortLabel>
               </TableCell>
             ))
           }
@@ -60,18 +74,47 @@ export default function useTable(records, headCells, filterFn) {
     setPage(0)
   }
 
-  const TablePaginationLol = () => (<TablePagination
-    component="div"
-    page={page}
-    rowsPerPageOptions={pages}
-    rowsPerPage={rowsPerPage}
-    count={records.length}
-    onChangePage={handleChangePage}
-    onChangeRowsPerPage={handleChangeRowsPerPage}
-  />)
+  const TablePaginationLol = () => (
+    <TablePagination
+      component="div"
+      page={page}
+      rowsPerPageOptions={pages}
+      rowsPerPage={rowsPerPage}
+      count={records.length}
+      onChangePage={handleChangePage}
+      onChangeRowsPerPage={handleChangeRowsPerPage}
+    />
+  )
+
+  const stableSort = (array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index])
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0])
+      if (order !== 0) return order
+      return a[1] - b[1]
+    })
+    return stabilizedThis.map((el) => el[0])
+  }
+
+  const getComparator = (order, orderBy) => {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy)
+  }
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1
+    }
+    return 0
+  }
 
   const recordsAfterPagingAndSorting = () => {
-    return records.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+    return stableSort(records, getComparator(order, orderBy))
+      .slice(page * rowsPerPage, (page + 1) * rowsPerPage)
   }
 
   return {
